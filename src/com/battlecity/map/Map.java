414253d7253db40cfc0d3c843d5be9345ace94d7
Umut Ertuğrul Daşgın <ertugruldasgin@hotmail.com>
2026-05-28 15:08:34 +0300

    feat: first map try

--- src/com/battlecity/map/Map.java
+++ src/com/battlecity/map/Map.java
@@ -1,7 +1,7 @@
 package com.battlecity.map;
 
 public class Map {
-	public static final int EDGE_LENGTH = 26;
+	public static final int EDGE_LENGTH = 13;
 	public static final int BOARD_WIDTH = 16 * EDGE_LENGTH;
 
 	private CellType[][] stage;
@@ -13,4 +13,22 @@ public class Map {
 	public CellType getCell(int row, int col) { return stage[row][col]; }
 
 	public void setCell(int row, int col, CellType val) { stage[row][col] = val; }
+
+	public void loadStage1() {
+		for (int r = 0; r < EDGE_LENGTH; r++) {
+			for (int c = 0; c < EDGE_LENGTH; c++) { stage[r][c] = CellType.EMPTY; }
+		}
+
+		stage[12][6] = CellType.EAGLE;
+
+		stage[12][5] = CellType.BRICK;
+		stage[12][7] = CellType.BRICK;
+		stage[11][5] = CellType.BRICK;
+		stage[11][6] = CellType.BRICK;
+		stage[11][7] = CellType.BRICK;
+
+		for (int i = 1; i < 6; ++i) { for (int j = 0; j < 6; ++j) { stage[i][j * 2 + 1] = CellType.BRICK; } }
+
+		for (int i = 7; i < 10; ++i) { for (int j = 0; j < 6; ++j) { stage[i][j * 2 + 1] = CellType.BRICK; } }
+	}
 }
--- src/com/battlecity/ui/GameWindow.java
+++ src/com/battlecity/ui/GameWindow.java
@@ -8,193 +8,72 @@ import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.battlecity.map.CellType;
+import com.battlecity.map.Map;
 import com.battlecity.util.SpriteLoader;
 
 public class GameWindow extends JFrame {
+	private Map map;
+
 	public GameWindow() {
 		setTitle("Battle City");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 
+		map = new Map();
+		map.loadStage1();
+
 		JPanel panel = new JPanel() {
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-//        	    BufferedImage brick = SpriteLoader.getBrick();
-//        	    BufferedImage brickRight = SpriteLoader.getBrickRight();
-//        	    BufferedImage brickBottom = SpriteLoader.getBrickBottom();
-//        	    BufferedImage brickLeft = SpriteLoader.getBrickLeft();
-//        	    BufferedImage brickTop = SpriteLoader.getBrickTop();
-//        	    BufferedImage miniBrick = SpriteLoader.getMiniBrick();
-//        	    BufferedImage miniBrickRight = SpriteLoader.getMiniBrickRight();
-//        	    BufferedImage miniBrickBottom = SpriteLoader.getMiniBrickBottom();
-//        	    BufferedImage miniBrickLeft = SpriteLoader.getMiniBrickLeft();
-//        	    BufferedImage miniBrickTop = SpriteLoader.getMiniBrickTop();
-//        	    
-//        	    BufferedImage steel = SpriteLoader.getSteel();
-//        	    BufferedImage steelRight = SpriteLoader.getSteelRight();
-//        	    BufferedImage steelBottom = SpriteLoader.getSteelBottom();
-//        	    BufferedImage steelLeft = SpriteLoader.getSteelLeft();
-//        	    BufferedImage steelTop = SpriteLoader.getSteelTop();
-//        	    BufferedImage miniSteel = SpriteLoader.getMiniSteel();
-//
-//				BufferedImage ice = SpriteLoader.getIce();
-//				BufferedImage miniIce = SpriteLoader.getMiniIce();
-//
-//				BufferedImage bush = SpriteLoader.getBush();
-//				BufferedImage miniBush = SpriteLoader.getMiniBush();
-//
-//				BufferedImage steelWall = SpriteLoader.getSteelWall();
-//				BufferedImage miniSteelWall = SpriteLoader.getMiniSteelWall();
-//
-//				BufferedImage eagleAlive = SpriteLoader.getEagleAlive();
-//				BufferedImage eagleDead = SpriteLoader.getEagleDead();
-//
-//				BufferedImage water = SpriteLoader.getWater();
-//				BufferedImage waterSec = SpriteLoader.getWaterSec();
-//				BufferedImage miniWater = SpriteLoader.getMiniWater();
-//				BufferedImage miniWaterSec = SpriteLoader.getMiniWaterSec();
-//
-//				BufferedImage[] spawn = SpriteLoader.getSpawn();
-//
-//				BufferedImage[] bullet = SpriteLoader.getBullet();
-//
-//				BufferedImage greenInk = SpriteLoader.getGreenInk();
-//
-//				BufferedImage[] powerUps = SpriteLoader.getPowerUps();
-//
-//				BufferedImage[] explosion = SpriteLoader.getExplosion();
-//
-//				BufferedImage[] miniExplosion = SpriteLoader.getMiniExplosion();
-//
-//				BufferedImage[] blinkEffect = SpriteLoader.getBlinkEffect();
-//
-//				BufferedImage[] scores = SpriteLoader.getScores();
-//
-//				BufferedImage pause = SpriteLoader.getPause();
-//				BufferedImage gameOver = SpriteLoader.getGameOver();
-//				BufferedImage stage = SpriteLoader.getStage();
-//
-//				BufferedImage enemyMark = SpriteLoader.getEnemyMark();
 
-				BufferedImage[] numbers = SpriteLoader.getNumbers();
+				CellType[][] grid = map.getMap();
+				int cellSize = 64;
 
-				BufferedImage grayBackground = SpriteLoader.getGrayBackground();
-				BufferedImage brownBackground = SpriteLoader.getBrownBackground();
-				BufferedImage orangeBackground = SpriteLoader.getOrangeBackground();
+				for (int i = 0; i < Map.EDGE_LENGTH; ++i) {
+					for (int j = 0; j < Map.EDGE_LENGTH; j++) {
+						CellType cell = grid[i][j];
+						BufferedImage sprite = null;
 
-				int x = 10, y = 10;
-//        	    g.drawImage(brick, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(brickRight, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(brickBottom, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(brickLeft, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(brickTop, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(miniBrick, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(miniBrickRight, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(miniBrickBottom, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(miniBrickLeft, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(miniBrickTop, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(steel, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(steelRight, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(steelBottom, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(steelLeft, x, y, 32, 32, null);
-//        	    y += 42;
-//        	    g.drawImage(steelTop, x, y, 32, 32, null);
-//           	y += 42;
-//        	    g.drawImage(miniSteel, x, y, 32, 32, null);
-//        	    y += 42;
-//				g.drawImage(ice, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(miniIce, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(bush, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(miniBush, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(steelWall, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(miniSteelWall, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(eagleAlive, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(eagleDead, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(water, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(waterSec, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(miniWater, x, y, 32, 32, null);
-//				y += 42;
-//				g.drawImage(miniWaterSec, x, y, 32, 32, null);
-//				for (int i = 0; i < 4; ++i) {
-//					y += 42;
-//					g.drawImage(spawn[i], x, y, 32, 32, null);
-//				}
-//				for (int i = 0; i < 4; ++i) {
-//					y += 42;
-//					g.drawImage(bullet[i], x, y, 32, 32, null);
-//				}
-//				y += 42;
-//				g.drawImage(greenInk, x, y, 32, 32, null);
-//				for (int i = 0; i < 7; ++i) {
-//					y += 42;
-//					g.drawImage(powerUps[i], x, y, 32, 32, null);
-//				}
-//
-//				for (int i = 0; i < 2; ++i) {
-//					y += 42;
-//					g.drawImage(explosion[i], x, y, 32, 32, null);
-//				}
-//
-//				for (int i = 0; i < 2; ++i) {
-//					y += 42;
-//					g.drawImage(blinkEffect[i], x, y, 32, 32, null);
-//				}
-//
-//				for (int i = 0; i < 5; ++i) {
-//					y += 42;
-//					g.drawImage(scores[i], x, y, 64, 32, null);
-//				}
-//
-//				y += 42;
-//				g.drawImage(pause, x, y, 64, 32, null);
-//				y += 42;
-//				g.drawImage(gameOver, x, y, 64, 64, null);
-//				y += 74;
-//				g.drawImage(stage, x, y, 64, 32, null);
-//				y += 42;
-//				g.drawImage(enemyMark, x, y, 32, 32, null);
-				y += 42;
-				for (int i = 0; i < 10; ++i) {
-					y += 42;
-					g.drawImage(numbers[i], x, y, 32, 32, null);
+						switch (cell) {
+						case BRICK:
+							sprite = SpriteLoader.getBrick();
+							break;
+						case STEEL:
+							sprite = SpriteLoader.getSteel();
+							break;
+						case WATER:
+							sprite = SpriteLoader.getWater();
+							break;
+						case BUSH:
+							sprite = SpriteLoader.getBush();
+							break;
+						case ICE:
+							sprite = SpriteLoader.getIce();
+							break;
+						case STEEL_WALL:
+							sprite = SpriteLoader.getSteelWall();
+							break;
+						case EAGLE:
+							sprite = SpriteLoader.getEagleAlive();
+							break;
+						default:
+							break;
+						}
+
+						if (sprite != null) {
+							g.drawImage(sprite, j * cellSize, i * cellSize, cellSize, cellSize, null);
+						}
+					}
 				}
-				y += 42;
-				g.drawImage(grayBackground, x, y, 64, 64, null);
-				y += 74;
-				g.drawImage(brownBackground, x, y, 64, 64, null);
-				y += 74;
-				g.drawImage(orangeBackground, x, y, 64, 64, null);
 			}
 		};
 
-		panel.setPreferredSize(new Dimension(400, 800));
-		panel.setBackground(Color.CYAN);
+		int windowSize = Map.EDGE_LENGTH * 64;
+		panel.setPreferredSize(new Dimension(windowSize, windowSize));
+		panel.setBackground(Color.BLACK);
 		add(panel);
-
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
